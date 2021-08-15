6d2fd35b43eb221c021acb73b2d8acbd6a36ddf6
surajpshenoy <surajpshenoy@gmail.com>
2021-08-15 18:19:48 +1000

    navbar links added

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const navBtn = document.getElementById("nav-toggle");
+const  navLinks = document.getElementById("nav-links")
+navBtn.addEventListener("click", () => {
+console.log("Button clicked")    
+})
+
